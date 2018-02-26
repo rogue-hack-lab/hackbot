9acce8ecd666c05f16c04653d2b55e0902a78fc2
Kevin Conner <kevin.r.conner@gmail.com>
2018-02-25 22:42:45 -0800

    converted tests from coffee to js

---
+++ test/uptime.spec.js
@@ -0,0 +1,22 @@
+const Helper = require('hubot-test-helper');
+const helper = new Helper('../scripts/uptime.coffee');
+
+const { expect } = require('chai');
+
+describe('uptime', () => {
+  
+  beforeEach( () => this.room = helper.createRoom() )
+  
+  afterEach( () => this.room.destroy() )
+
+  it('seconds', () => {
+    this.room.user.say('kevin', '@hubot uptime')
+    .then( () => {
+      expect (this.room.messages).to.eql([
+        ['kevin', '@hubot uptime'],
+        ['hubot', 'I\'ve been sentient for ']
+      ] )
+    })
+  }); 
+
+});
