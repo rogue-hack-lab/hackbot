ea8818c38e0613681248998b825ece7482c7bee2
Patrick Cunningham <pcunning@users.noreply.github.com>
2020-10-25 15:12:15 -0700

    Update enterReplies.js

--- stubs/enterReplies.js
+++ stubs/enterReplies.js
@@ -6,14 +6,12 @@ icebreakers = [
   'What\'s one skill you would like to develop?',
   'What\'s your favorite letter of the alphabet and why?',
   'How old were you when you first used a computer?',
-  'What\'s your favorite spot for a beer?',
   'How did you hear about the hack lab?',
   'When was the last time you changed your hairstyle?',
   'Have you committed today?',
   'You\'re trapped on a deserted island, what TV show are you bringing to keep you company?',
   'What\'s your favorite flavor of sparkling water?',
   'Tell us your favorite \'nerdy\' joke',
-  'How old were you when you stopped believing in Santa Claus?',
   'Share your favorite color (in hexcode!)',
   'What website do you suddenly find yourself on when you\'re \"just checking your email\"?',
   'If you could be any other species, which would you choose?',
@@ -22,6 +20,6 @@ icebreakers = [
 ];
 
 // Combine introduction with an icebreaker question
-responses = icebreakers.map((item) => introduction + ' ' + item + 'Start a thread with your response to get to know the hack lab better');
+responses = icebreakers.map((item) => introduction + ' ' + item + ' Start a thread with your response to get to know the hack lab better');
 
 module.exports = responses;
